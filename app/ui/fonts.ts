84d621cd28701f78fcf6fa700011d0376a24ec93
Nareph <nmenadjou@iotaorigin.de>
2024-05-16 13:19:21 +0100

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { David_Libre, Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+export const david_libre = David_Libre({
+    weight: ['400', '500', '700'],
+    subsets: ['latin'],
+});
