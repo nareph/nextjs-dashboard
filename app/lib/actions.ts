929e86dd78bd2eff5dd2e8a09acff78cdcbef78e
Nareph <nmenadjou@iotaorigin.de>
2024-05-18 16:32:57 +0100

    delete the unnecessary line in the code

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -108,9 +108,6 @@ export async function updateInvoice(
 
 export async function deleteInvoice(id: string) {
   try {
-
-    throw new Error('Failed to Delete Invoice');
-
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
     return { message: 'Deleted Invoice.' };
